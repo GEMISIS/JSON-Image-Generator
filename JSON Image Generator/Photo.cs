671aab968517fb1a2eaa161b04b0f7eff2e57802
Gerald McAlister <gerald.mcalister.gem@gmail.com>
2014-08-29 12:52:32 -0400

    Initial Creation
    -Created the initial project
    -Added external library to easily handle JSON in C#
    -Started adding photo JSON parser object.

---
+++ JSON Image Generator/Photo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JSON_Image_Generator
+{
+    class Photo
+    {
+        private Image image;
+        private string filePath;
+
+        private string pictureName;
+        private string pictureDescription;
+        private int width, height;
+
+        public Photo(string filePath)
+        {
+            this.image = Image.FromFile(filePath);
+            
+            this.filePath = filePath;
+            
+            this.pictureName = filePath.Substring(filePath.LastIndexOf("/") + 1);
+            this.pictureDescription = "";
+
+            this.width = this.image.Width;
+            this.height = this.image.Height;
+        }
+
+        public void ReadJSON(JsonTextReader reader)
+        {
+        }
+
+        public void WriteJSON(JsonTextWriter writer)
+        {
+            writer.WriteStartObject();
+
+            writer.WritePropertyName("filePath");
+            writer.WriteValue(this.filePath);
+
+            writer.WriteEndObject();
+        }
+
+        public string Path
+        {
+            get
+            {
+                return this.filePath;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.pictureName;
+            }
+            set
+            {
+                this.pictureName = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return this.pictureDescription;
+            }
+            set
+            {
+                this.pictureDescription = value;
+            }
+        }
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return this.height;
+            }
+        }
+    }
+}
---
+++ JSON Image Generator/mainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace JSON_Image_Generator
+{
+    public partial class mainForm : Form
+    {
+        private JsonTextReader jsonReader;
+        private JsonTextWriter jsonWriter;
+
+        private FolderBrowserDialog folderBrowser;
+
+        private OpenFileDialog openJsonFileDialog;
+        private SaveFileDialog saveJsonFileDialog;
+
+        public mainForm()
+        {
+            InitializeComponent();
+
+            folderBrowser = new FolderBrowserDialog();
+
+            openJsonFileDialog = new OpenFileDialog();
+            openJsonFileDialog.Filter = "JSON Files (.json)|*.json";
+            openJsonFileDialog.FileOk += openJsonFileDialog_FileOk;
+
+            saveJsonFileDialog = new SaveFileDialog();
+            saveJsonFileDialog.Filter = "JSON Files (.json)|*.json";
+            saveJsonFileDialog.FileOk += saveJsonFileDialog_FileOk;
+        }
+
+        void openJsonFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            OpenFileDialog dialog = sender as OpenFileDialog;
+            jsonReader = new JsonTextReader(new StringReader(File.ReadAllText(dialog.FileName)));
+        }
+
+        void saveJsonFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openJsonFileDialog.ShowDialog();
+        }
+    }
+}
